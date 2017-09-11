2f6abbf67aa8b2eed6f4e842638822bb4ddbc68b
2018deckerd <31222254+2018deckerd@users.noreply.github.com>
2017-09-11 09:04:50 -1000

    Add files via upload

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -5,14 +5,38 @@
  */
 public class SavingsAccount extends BankAccount {
 	
+	
+	static double hold;
+	static double interestRate = 6;
+	
 
-	public SavingsAccount(int accountNumber, double amount) {
-		super(accountNumber, amount);
+	public SavingsAccount(String name, int accountNumber) {
+		super(name, accountNumber);
 	
 	}
+	
+	public static double returnMinimum(double num1, double num2) {
+		double hold = 0;
+		 for (int i = 0; i < 2; i++) {
+			hold = num1;
+			if (num2 < num1) {
+				hold = num2;
+			} else {
+				hold = num1;
+			}
+		 }
+		return hold;
+	 
+	}
 
+	public static double computeInterest(double minimum) {
+		double value = (minimum * interestRate) / 100;
+		return value;
+	
+}
 
-
+	
+	
 	public void monthEnd() {
 		
 	}
